9cd0a22c0e454c61b481d8448b53708d85a1b067
ontoligent <ontoligent@gmail.com>
2018-09-20 12:26:02 -0400

    Redesigned front page

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
-echo "Building xom-paragraphs.html . . ."
+echo "Applying XSLT to xom-paragraphs.html ..."
 xsltproc xom-paragraphs.xsl xom-all-flat-mod-pnums.xml > xom-paragraphs.html; 
+
+echo "Applying replacements ..."
 ./xom-paragraphs.py
+
 echo "Done."
