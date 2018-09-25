40178badbdfdece8dcb689d3a7215dc7882f4741
ontoligent <ontoligent@gmail.com>
2018-09-25 16:03:04 -0400

    Copied bootstrap files locally

--- build-edition-paragraphs.sh
+++ build-edition-paragraphs.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-echo "Applying XSLT ..."
+echo "Applying XSLT to paragraphs ..."
 xsltproc xom-paragraphs.xsl xom-all-flat-mod-pnums.xml > ./templates/xom-paragraphs.html;
 
 echo "Applying replacements ..."
