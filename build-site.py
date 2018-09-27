dcb235e0ad3bb4b8ed919cee0e008026d3771815
ontoligent <ontoligent@gmail.com>
2018-09-27 11:06:13 -0400

    Using local assets

--- build-site.py
+++ build-site.py
@@ -15,39 +15,23 @@ ENV = Environment(loader=FileSystemLoader('./templates'))
 scripts = dict()
 styles = dict()
 scripts['index'] = [
-    dict(src="https://code.jquery.com/jquery-3.3.1.slim.min.js",
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
-        crossorigin="anonymous"),
-    dict(src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49",
-        crossorigin="anonymous"),
-    dict(src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js",
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy",
-        crossorigin="anonymous")
+    dict(src="assets/jquery-3.3.1.slim.min.js"),
+    dict(src="assets/popper.min.js"),
+    dict(src="assets/bootstrap.min.js")
 ]
 styles['index'] = [
-    dict(href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO",
-        crossorigin="anonymous"),
+    dict(href="assets/bootstrap.min.css"),
     dict(href="pw-default.css")
 ]
 scripts['paragraphs'] = [
-    dict(src="https://code.jquery.com/jquery-3.3.1.slim.min.js",
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
-        crossorigin="anonymous"),
-    dict(src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49",
-        crossorigin="anonymous"),
-    dict(src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js",
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy",
-        crossorigin="anonymous"),
-    dict(src="./xom-paragraphs.js", crossorigin="anonymous")
+    dict(src="assets/jquery-3.3.1.slim.min.js"),
+    dict(src="assets/popper.min.js"),
+    dict(src="assets/bootstrap.min.js"),
+    dict(src="./xom-paragraphs.js")
 ]
 styles['paragraphs'] = [
-    dict(href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO",
-        crossorigin="anonymous"),
-    dict(href="./xom-paragraphs.css", crossorigin="anonymous")
+    dict(href="assets/bootstrap.min.css"),
+    dict(href="./xom-paragraphs.css")
 ]
 docmap = {
     'index.html': {
