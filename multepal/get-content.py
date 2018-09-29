316c8ba7a8e5e794a064973fa9117033b0c06275
ontoligent <ontoligent@gmail.com>
2018-09-29 18:50:14 -0400

    Fixed annotations pull

--- multepal/get-content.py
+++ multepal/get-content.py
@@ -28,5 +28,82 @@ for export in exports.keys():
         out.write(r.text)
 
 # Post processing
-# Todo: handle this more systematically
-os.system(base_path + 'multepal/annotations.py')
+
+import re
+import xml.etree.ElementTree as ET
+
+file = base_path + "multepal/annotations.xml"
+tree = ET.parse(file)
+root = tree.getroot()
+
+amap = ET.SubElement(root, 'annotation-map')
+
+def reformat_lb_id(lb_id):
+    lb_id = re.sub('Folio-', 'xom-f', lb_id)
+    lb_id = re.sub('recto', 's1', lb_id)
+    lb_id = re.sub('verso', 's2', lb_id)
+    lb_id = re.sub(' Column-A', '-quc', lb_id)
+    lb_id = re.sub(' Column-B', '-spa', lb_id)
+    lb_id = re.sub(' Line', '', lb_id)
+    lb_id = re.sub('Escolio-', 'xom_esc-f', lb_id)
+    lb_id = re.sub(' X', '', lb_id)
+    return lb_id
+
+for note in root.iter('annotation'): 
+    note.set('nid', note.find('nid').text)
+    lines = note.find('lineas')
+    if lines.text == None:
+        continue
+
+    F = C = L = ''
+    newlines = {}
+    for item in lines.text.split('|'):
+        tokens = item.split()
+        key = tokens[0]
+        val = '-'.join(tokens)
+        if key in ('Folio', 'Escolio'):
+            if val != F:
+                F = val
+                C = ''
+                L = ''
+                newlines[F] = {}
+                if key == 'Escolio':
+                    newlines[F]['X'] = []
+        elif key == 'Column':
+            if val != C:
+                C = val
+                L = ''
+                newlines[F][C] = []
+        elif key == 'Line':
+            if L == val:
+                continue
+            L = val
+            if C == '':
+                C = 'X'
+            Ln = int(L.split('-')[1])
+            newlines[F][C].append('Line-{}'.format(Ln))
+            
+    newlineids = []            
+    for folio in newlines:
+        for col in newlines[folio]:
+            if len(newlines[folio][col]) == 0:
+                lb_id = "{} {}".format(folio, col)
+                newlineids.append(reformat_lb_id(lb_id))
+            for line in newlines[folio][col]:
+                lb_id = "{} {} {}".format(folio, col, line)
+                newlineids.append(reformat_lb_id(lb_id))
+
+    #lines.text = ''
+    for line_id in newlineids:
+        #X = ET.SubElement(lines, 'line_id')
+        #X.text = line_id
+        Y = ET.SubElement(amap, 'item')
+        Y.set('nid', note.find('nid').text)
+        Y.set('line_id', line_id)
+    lines.set('reformatted','true')
+
+    for el_name in ['nid', 'lineas']:
+        el = note.find(el_name)
+        note.remove(el)
+
+tree.write(file, method='xml', encoding='utf-8')
