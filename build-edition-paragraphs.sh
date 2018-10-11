326594986fcb6a7b6e88715df0d9f22019ce2f26
ontoligent <ontoligent@gmail.com>
2018-10-11 11:45:43 -0400

    Replaced xsltproc with saxon

--- build-edition-paragraphs.sh
+++ build-edition-paragraphs.sh
@@ -4,7 +4,8 @@ echo "Add line ids to TEI source ..."
 ./xom-all-flat-mod-pnums.py;
 
 echo "Applying XSLT to paragraphs ..."
-xsltproc xom-paragraphs.xsl xom-all-flat-mod-pnums-lbids.xml > ./templates/xom-paragraphs.html;
+#xsltproc xom-paragraphs.xsl xom-all-flat-mod-pnums-lbids.xml > ./templates/xom-paragraphs.html;
+saxon xom-all-flat-mod-pnums-lbids.xml xom-paragraphs.xsl > ./templates/xom-paragraphs.html;
 
 echo "Applying replacements ..."
 ./xom-paragraphs.py ./templates/xom-paragraphs.html;
